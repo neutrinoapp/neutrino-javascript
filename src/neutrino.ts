3d1d5d0f93cc5d1c76dad09ae12b4aeb0fb72fa5
Petar Krastanov <peter.b.krustanov@gmail.com>
2016-03-16 23:36:03 +0200

    Added mocha and chai for the unit test

--- src/neutrino.ts
+++ src/neutrino.ts
@@ -30,9 +30,9 @@ export class App {
         private opts?: AppOptions
     ) {
         opts = opts || {};
-        this.host = opts.host || 'http://localhost:5000/v1/';
+        this.host = opts.host || 'http://104.155.97.49/v1/';
         this.appHost = this.host + 'app/' + this.appId + '/';
-        this.realtimeHost = opts.realtimeHost || 'ws://localhost:6000/';
+        this.realtimeHost = opts.realtimeHost || 'ws://23.251.130.65/';
 
         this.auth = new Authentication(this);
     }
@@ -56,7 +56,7 @@ App['ArrayEvents'] = ArrayEvents;
 if (typeof window !== 'undefined') {
     window['Neutrino'] = App;
 } else if (typeof module !== 'undefined') {
-    module['exports'] = App;
+    module['Neutrino'] = App;
 } else {
     this['Neutrino'] = App;
 }
